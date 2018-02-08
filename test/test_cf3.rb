84dfd410d432eff80b485dfe3564a22f28fa1ff9
monkstone <mamba2928@yahoo.co.uk>
2018-02-08 16:00:42 +0000

    show center

--- test/test_cf3.rb
+++ test/test_cf3.rb
@@ -1,21 +1,22 @@
 require_relative '../lib/cf3.rb'
 
 def settings
-  size 200, 200 
+  size 200, 200
 end
 
 def setup
+  sketch_title 'Test'
   @triangle = ContextFree.define do
     shape :tri do
-      circle      
+      circle
       triangle brightness: 0.8
-      circle brightness: 1.0, size: 0.02
+      square brightness: 0.02, size: 0.02
     end
-  end 
+  end
 end
 
 def draw_it
-  background 0.2 
+  background 0.2
   @triangle.render :tri, size: height/2
 end
 
